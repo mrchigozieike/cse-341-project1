2408108764cb8d78465323c798f1dfc6dc122eb5
mrchigozieike <70382310+mrchigozieike@users.noreply.github.com>
2024-09-29 05:11:42 +0100

    project1 complete

--- controllers/users.js
+++ controllers/users.js
@@ -4,22 +4,27 @@ const ObjectId = require('mongodb').ObjectId;
 // Get all users
 const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) =>{
-        res.setHeader('content-type', 'application/json');
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
     });
-
 };
 
 // Get a single user by ID
-const getSingle = async (req, res) => {    
-    const userId = new ObjectId(req.prams.id);
-    const result = await mongodb.getDatabase().db().collection('users').find();
-     
-    result.toArray().then((users) =>{
+const getSingle = async (req, res) => {
+    try {
+        const userId = new ObjectId(req.params.id); 
+        const result = await mongodb.getDatabase().db().collection('users').findOne({ _id: userId });
+        
+        if (!result) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        
         res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(users[0]);
-        });
+        res.status(200).json(result); // Return the found user
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 module.exports = {
